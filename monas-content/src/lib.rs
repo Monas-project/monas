baaf24ce7164da336a0b7532b7f93bc66b361c9d
yuseirf <82315017+YuseiWhite@users.noreply.github.com>
2025-04-23 17:41:12 +0900

    [Content] Add monas-content/src/lib.rs

---
+++ monas-content/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod domain;
+pub mod infrastructure;
+pub mod presentation;
+pub mod application_service;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
